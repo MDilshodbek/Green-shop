be65ea19bd9d76af6a13e77e3983a05eb2e9810d
MDilshodbek <mdbek55@gmail.com>
2024-03-05 19:18:02 +0900

    solved in different method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 64bebc1e2c6d3f056a8c85b7 token
-// let url = "https://cute-jade-binturong-boot.c yclic.app/api";
+// let url = "https://cute-jade-binturong-boot.cyclic.app/api";
 
 // let getflowers = (search_flower) => {
 //   fetch(
@@ -25,20 +25,54 @@
 
 // getflowers("Cactus");
 
-let url = "https://cute-jade-binturong-boot.c yclic.app/api";
+// ASYNC AWAIT Method
 
-const getflowers = async (search_flower) => {
-  const response = await fetch(
-    `${url}/flower/category/house-plants?access_token=64bebc1e2c6d3f056a8c85b7`,
-  );
+// let url = "https://cute-jade-binturong-boot.cyclic.app/api";
 
-  const data = await response.json();
-  console.log(data);
-if(founddata.length === 0) {
-    console.log("No flowers found");
-  } else {
-    console.log(founddata);
+// const getflowers = async (search_flower) => {
+//   const response = await fetch(
+//     `${url}/flower/category/house-plants?access_token=64bebc1e2c6d3f056a8c85b7`
+//   );
+
+//   const data = await response.json();
+
+//   const founddata = data.data.filter((value) => {
+//     return value.title.includes(search_flower)
+//   })
+
+//   if (founddata.length === 0) {
+//     console.log("No flowers found");
+//   } else {
+//     console.log(founddata);
+//   }
+// };
+
+// getflowers("Cactus");
+
+let url = "https://cute-jade-binturong-boot.cyclic.app/api";
+
+let signup = () => {
+  fetch(`${url}/user/sign-up?access_token=64bebc1e2c6d3f056a8c85b7`, {
+    method: "POST",
+    body: JSON.stringify({
+      name: "James",
+      surname: "Bond",
+      password: "12345678",
+      email: "agent007@gmail.com",
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
-}
 
-getflowers("Cactus");
+signup();
+
+
+
